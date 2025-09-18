c7dbcc9c8c744158e7cf08714b50d315534cc71c
悲喜 <741460862@qq.com>
2025-09-18 15:21:25 +0800

    feat: change title

--- src/app/api/scanner/route.ts
+++ src/app/api/scanner/route.ts
@@ -7,26 +7,26 @@ import type { ScannerResponse } from '@t/scanner'
 
 export async function GET() {
     try {
-        const Scanner = await prisma.t_ScanRecords_Boards.findFirst({
-            orderBy: { Id: 'desc' }
-        })
-        let LastDate = Scanner.ScanTime==null? dayjs(Scanner.ScanTime).format('YYYY-MM-DD hh:mm:ss').toString():dayjs().format('YYYY-MM-DD hh:mm:ss').toString();
-        const BoardInfo = await prisma.t_BoardInfo.findFirst({ where: { UPID: Scanner.Upi } })
-        const response: ScannerResponse = {
-            data: {
-                id: Scanner.Id,
-                Batch: BoardInfo.BatchId,
-                Length: BoardInfo.Length,
-                Width: BoardInfo.Width,
-                Thickness: BoardInfo.Thickness,
-                UPID: Scanner.Upi,
-                LastDate,
-                Consistency: 1
-            },
-            success: true,
-            message: BoardInfo ? undefined : '未找到数据'
-        }
-        return NextResponse.json(response)
+        // const Scanner = await prisma.t_ScanRecords_Boards.findFirst({
+        //     orderBy: { Id: 'desc' }
+        // })
+        // let LastDate = Scanner.ScanTime==null? dayjs(Scanner.ScanTime).format('YYYY-MM-DD hh:mm:ss').toString():dayjs().format('YYYY-MM-DD hh:mm:ss').toString();
+        // const BoardInfo = await prisma.t_BoardInfo.findFirst({ where: { UPID: Scanner.Upi } })
+        // const response: ScannerResponse = {
+        //     data: {
+        //         id: Scanner.Id,
+        //         Batch: BoardInfo.BatchId,
+        //         Length: BoardInfo.Length,
+        //         Width: BoardInfo.Width,
+        //         Thickness: BoardInfo.Thickness,
+        //         UPID: Scanner.Upi,
+        //         LastDate,
+        //         Consistency: 1
+        //     },
+        //     success: true,
+        //     message: BoardInfo ? undefined : '未找到数据'
+        // }
+        // return NextResponse.json(response)
     } catch (error) {
         const response: ScannerResponse = {
             data: null,
